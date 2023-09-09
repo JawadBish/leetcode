b95d7db97c86158b9449201381fe835bcadd7406
NBU Builder <nbubuilder3@amdocs.com>
2023-09-09 18:06:55 +0300

    solve 1768. Merge Strings Alternately

---
+++ training/java/src/main/java/Easy/MergeStringsAlternately.java
@@ -0,0 +1,25 @@
+package Easy;
+
+public class MergeStringsAlternately {
+
+    public static String mergeAlternately(String word1, String word2) {
+       StringBuilder result = new StringBuilder();
+       int i = 0;
+       while (i < word1.length() || i < word2.length()) {
+           if ( i < word1.length()) {
+            result.append(word1.charAt(i));
+           }
+           if ( i < word2.length()) {
+               result.append(word2.charAt(i));
+           }
+        i++;
+       }
+       return result.toString();
+        }
+
+    public static void main(String[] args) {
+        String w1 = "ab";
+        String w2 = "pq5";
+        System.out.print(mergeAlternately(w1,w2));
+    }
+}
